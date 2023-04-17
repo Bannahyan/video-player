9b7f837fd02c261a52a7de15e192d9a74f7ec196
Bannahyan <anna.bannahyan@gmail.com>
2023-04-17 20:34:06 +0100

    feat: add permissions policy config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'fullscreen=(self)',
+          },
+        ],
+        source: '/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
