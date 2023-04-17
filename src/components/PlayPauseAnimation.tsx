e32368472977f41a80ff80d19cffff91d55d1889
Bannahyan <anna.bannahyan@gmail.com>
2023-04-17 10:11:11 +0100

    feat: add play/pause animation

---
+++ src/components/PlayPauseAnimation.tsx
@@ -0,0 +1,38 @@
+import classNames from 'classnames';
+import { AiFillCaretRight, AiOutlinePause } from 'react-icons/ai';
+import styles from '../styles/home.module.css';
+
+interface PlayPauseProps {
+  areaClicked: boolean;
+  setAreaClicked: Function;
+  handleTogglePlay: Function;
+  isPaused: boolean;
+}
+
+const PlayPause = ({
+  areaClicked,
+  setAreaClicked,
+  handleTogglePlay,
+  isPaused,
+}: PlayPauseProps) => {
+  return (
+    <div
+      className={classNames(
+        styles.playPauseIcon,
+        areaClicked ? styles.visible : styles.hidden
+      )}
+      onClick={() => {
+        setAreaClicked(true);
+        handleTogglePlay();
+      }}
+    >
+      {isPaused ? (
+        <AiOutlinePause color='white' size={24} />
+      ) : (
+        <AiFillCaretRight color='white' size={24} />
+      )}
+    </div>
+  );
+};
+
+export default PlayPause;
