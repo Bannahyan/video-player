aea4adca9fea21f47097a4d7c20f3ebdfad5cd3a
Bannahyan <anna.bannahyan@gmail.com>
2023-04-23 11:17:01 +0100

    feat: remove not used imports

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState, useCallback } from 'react';
-import Hls from 'hls.js';
 import Controls from './ControlsPanel';
 import ForwardBackward from './ForwardBackwardLayer';
 import PlayPause from './PlayPauseAnimation';
